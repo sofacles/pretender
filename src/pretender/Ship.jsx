dff299eac51873a376c01806f4ce7f66d9f3f7d2
sofacles <sofacles@yahoo.com>
2024-12-06 10:19:59 -0800

    adding a diagnostic text element to the ship cuz it's not going all the way to the top of the screen

--- src/pretender/Ship.jsx
+++ src/pretender/Ship.jsx
@@ -45,7 +45,10 @@ const Ship = ({ isThrusting, x, y, reversed }) => {
     return () => {clearInterval(killInterval)}
   }, []);
   return (
-    <g transform={`translate(${x},${y}) scale(0.25) ${reversed ? "scale(-1, 1)" : ""}`}>
+    <g transform={`translate(${x},${y}) scale(0.25)`}>
+      <text className="my-text">{`y: ${y}`}</text>
+      <g transform={`${reversed ? "scale(-1, 1)" : ""}`}>
+      
       {isThrusting &&
         <g>
           <path d="M200,100l100,0 0,5 -100,0 z" fill="orange" opacity={0.8} />
@@ -61,6 +64,7 @@ const Ship = ({ isThrusting, x, y, reversed }) => {
       }
       <path d="M156,108l3,-3 9,-1 1,-4 3,-4 -5,-7 -5,-7 0,1 -6,-1 -1,-4 -5,-8 -16,1 -3,7 -8,1 -2,6 -12,14 -39,-1 -1,11 -43,1 -9,3 1,9 9,4 17,0 1,0 8,1 13,0 22,-2 7,10 16,5 16,-6 14,-1 z" fill="#acafa5" />
       <path d="M120,114l20,2 10,-11 7,-2 9,5 18,3 -8,9 -13,2 -11,11 -16,1 0,-10 z" fill="#995997" />
+    </g>
     </g>
   );
 };
