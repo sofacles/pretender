3f06f126177c2756490733023d18ff0ed7e388c2
sofacles <sofacles@yahoo.com>
2024-12-07 18:10:38 -0800

    make the ship's path start at 80,50 rather than 200,100

--- src/pretender/Ship.jsx
+++ src/pretender/Ship.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const Ship = ({ isThrusting, x, y, reversed }) => {
+const Ship = ({ isThrusting, x, y, reversed, showMarkers }) => {
   const [colorIndex_top1, setcolorIndex_top1] = useState(0);
   const [colorIndex_top2, setcolorIndex_top2] = useState(0);
 
@@ -18,7 +18,8 @@ const Ship = ({ isThrusting, x, y, reversed }) => {
 
   const [randomX_bottom2, setRandomX_bottom2] = useState(0);
   const [randomY_bottom2, setrandomY_bottom2] = useState(0);
-
+  const TOP_LEFT_X = 80;
+  const TOP_LEFT_Y = 50;
 
   const colors = ["#c3eb34", "#eb8934", "#bf7b3f", "#f5f4ed", "#ff0101", "orange"];
 
@@ -27,32 +28,30 @@ const Ship = ({ isThrusting, x, y, reversed }) => {
  
     const killInterval = setInterval(() => {
       setcolorIndex_top1(Math.floor(Math.random() * 6));
-      setRandomX_top1(Math.floor(200 + Math.random() * 99));
-      setrandomY_top1(100 + Math.floor(Math.random() * 4));
+      setRandomX_top1(Math.floor(TOP_LEFT_X + Math.random() * 99));
+      setrandomY_top1(TOP_LEFT_Y + Math.floor(Math.random() * 4));
 
       setcolorIndex_top2(Math.floor(Math.random() * 6));
-      setRandomX_top2(Math.floor(200 + Math.random() * 101));
-      setrandomY_top2(100 + Math.floor(Math.random() * 4));
+      setRandomX_top2(Math.floor(TOP_LEFT_X + Math.random() * 101));
+      setrandomY_top2(TOP_LEFT_Y + Math.floor(Math.random() * 4));
 
       setcolorIndex_bottom1(Math.floor(Math.random() * 6));
-      setRandomX_bottom1(Math.floor(200 + Math.random() * 99));
-      setrandomY_bottom1(100 + Math.floor(Math.random() * 4));
+      setRandomX_bottom1(Math.floor(TOP_LEFT_X + Math.random() * 99));
+      setrandomY_bottom1(TOP_LEFT_Y + Math.floor(Math.random() * 4));
       
       setcolorIndex_bottom2(Math.floor(Math.random() * 6));
-      setRandomX_bottom2(Math.floor(200 + Math.random() * 101));
-      setrandomY_bottom2(110 + Math.floor(Math.random() * 4));
+      setRandomX_bottom2(Math.floor(TOP_LEFT_X + Math.random() * 101));
+      setrandomY_bottom2(TOP_LEFT_Y + Math.floor(Math.random() * 4));
     }, 100);
     return () => {clearInterval(killInterval)}
   }, []);
   return (
     <g transform={`translate(${x},${y}) scale(0.25)`}>
-      <text className="my-text">{`y: ${y}`}</text>
+      {showMarkers && <text x="0" y="0" className="horizontal-rect">top</text>}
       <g transform={`${reversed ? "scale(-1, 1)" : ""}`}>
       
       {isThrusting &&
         <g>
-          <path d="M200,100l100,0 0,5 -100,0 z" fill="orange" opacity={0.8} />
-          <path d="M200,110l100,0 0,5 -100,0 z" fill="orange" opacity={0.8} />
           <g>
             <path d={`M${randomX_top1},${randomY_top1}l8,0 0,2, -8,0 z`} fill={`${colors[colorIndex_top1]}`} />
             <path d={`M${randomX_top2},${randomY_top2}l6,0 0,2, -8,0 z`} fill={`${colors[colorIndex_top2]}`} />
@@ -62,9 +61,11 @@ const Ship = ({ isThrusting, x, y, reversed }) => {
           </g>
         </g>
       }
-      <path d="M156,108l3,-3 9,-1 1,-4 3,-4 -5,-7 -5,-7 0,1 -6,-1 -1,-4 -5,-8 -16,1 -3,7 -8,1 -2,6 -12,14 -39,-1 -1,11 -43,1 -9,3 1,9 9,4 17,0 1,0 8,1 13,0 22,-2 7,10 16,5 16,-6 14,-1 z" fill="#acafa5" />
-      <path d="M120,114l20,2 10,-11 7,-2 9,5 18,3 -8,9 -13,2 -11,11 -16,1 0,-10 z" fill="#995997" />
+      <path d="M36,58l3,-3 9,-1 1,-4 3,-4 -5,-7 -5,-7 0,1 -6,-1 -1,-4 -5,-8 -16,1 -3,7 -8,1 -2,6 -12,14 -39,-1 -1,11 -43,1 -9,3 1,9 9,4 17,0 1,0 8,1 13,0 22,-2 7,10 16,5 16,-6 14,-1 z" fill="#acafa5" />
+      <path d="M0,64l20,2 10,-11 7,-2 9,5 18,3 -8,9 -13,2 -11,11 -16,1 0,-10 z" fill="#995997" />
+     
     </g>
+    {showMarkers && <text x="200" y="200" className="my-text">{`y: ${y}`}</text>}
     </g>
   );
 };
