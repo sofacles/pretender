441c84748ef9022b4c595be50635a870ae775592
sofacles <sofacles@yahoo.com>
2025-02-15 19:00:20 -0800

    basic throbbing

--- src/pretender/KeyMappingEditor.tsx
+++ src/pretender/KeyMappingEditor.tsx
@@ -13,23 +13,23 @@ const KeyMappingEditor = (props: KeyMappingEditorProps) => {
   const { toggleEditMode, isEditing, isGlowing, keyMapping } = props;
   const { name, mappedKey } = keyMapping;
   const normalizeKeyName = (key: string) => {
-    
-    switch(key) {
-      case  " ":
+
+    switch (key) {
+      case " ":
         return "space";
-      
+
     }
-    
+
     return key;
   };
 
   return (
     <div className="h-center">
       <div className={isEditing ? "key-mapping-row-editing" : "key-mapping-row"}>
-        
+
         {!isEditing && (
           <>
-             <span>{name}</span><input type="text" className="key-mapping-read-only" disabled value={normalizeKeyName(mappedKey)} />
+            <span>{name}</span><input type="text" className="key-mapping-read-only" disabled value={normalizeKeyName(mappedKey)} />
             <button
               onClick={() => {
                 toggleEditMode(keyMapping);
@@ -39,10 +39,9 @@ const KeyMappingEditor = (props: KeyMappingEditorProps) => {
             </button>
           </>
         )}
-        {isEditing && !isGlowing && <>
+        {isEditing && !isGlowing &&
           <div className="throbbing-child">Press the new key for {name}</div>
-          <div style={{height: "50px", width: "100px"}}><input type="text" value="" /></div>
-          </>}
+        }
         {isGlowing && <div>{normalizeKeyName(mappedKey)} check!</div>}
       </div>
     </div>
